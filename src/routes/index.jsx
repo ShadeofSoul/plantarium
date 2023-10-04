34a44d70de239074573a987c4244cdab5f8cb69a
Shade Soul <aijanmoinokova@gmail.com>
2023-10-04 23:47:04 +0300

    added new page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
 import { lazy } from "react";
 import { useRoutes } from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 
 export default function Router() {
@@ -8,6 +10,9 @@ export default function Router() {
       path: "/",
       element: <Home />,
     },
+    { path: 'register', element: <Register /> },
+    { path: 'login', element: <Login /> },
+  
   ]);
 }
 
