929408387033faf6147fb19278c6ddcaa20579d7
Shade Soul <aijanmoinokova@gmail.com>
2023-10-04 23:50:41 +0300

    fixed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,13 @@
 import { lazy } from "react";
 import { useRoutes } from "react-router-dom";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
-
-
 export default function Router() {
   return useRoutes([
     {
       path: "/",
       element: <Home />,
     },
-    { path: 'register', element: <Register /> },
-    { path: 'login', element: <Login /> },
-  
+    { path: "register", element: <Register /> },
+    { path: "login", element: <Login /> },
   ]);
 }
 
