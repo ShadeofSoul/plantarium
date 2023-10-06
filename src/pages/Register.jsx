3ee584432c9b6022080c98b31ea798b7580c8f5c
Shade Soul <aijanmoinokova@gmail.com>
2023-10-06 22:24:00 +0300

    added RegisterForm

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import RegisterForm from "../components/auth/RegisterForm";
 
 const Register = () => {
-    return (
-        <div>
-            Register Page 
-        </div>
-    );
+  return <RegisterForm />;
 };
 
 export default Register;
