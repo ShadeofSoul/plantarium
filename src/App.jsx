7fbbc8bf8e9bd14df5c90c909f45dfc3100196e1
Shade Soul <aijanmoinokova@gmail.com>
2023-10-07 21:48:47 +0300

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+import AuthContextProvider from "./contexts/AuthContext";
 import Router from "./routes";
 import theme from "./theme/theme";
-import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
